203e4b971fef17caade4e3549a81c84e1b43bf92
DimitriosNicolay <hey@dnicolay.de>
2025-03-11 10:12:28 +0100

    adding previous Codesnippets

---
+++ konstanten.js
@@ -0,0 +1,7 @@
+let nummer = 10;
+
+const ergebnis = nummer * 2;
+
+nummer = 30;
+
+console.log(ergebnis);
---
+++ schleifen.js
@@ -0,0 +1,26 @@
+// let i = 0;
+
+// while (i < 10) {
+//     console.log(i),
+//     i++;
+// }
+
+// while (true) {
+//     console.log(i);
+//     i++;
+
+//     if (i >= 10) {
+//         break;
+//     }
+// }
+
+// console.log("Endergebnis: " + i);
+
+// do {
+//    console.log(i);
+//    i++;
+// } while (i <10);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
